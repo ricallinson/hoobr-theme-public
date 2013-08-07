ea4e81aa25a16a9866dc59867b3179bc82f7844c
Ric Allinson <capecodehq@yahoo.co.uk>
2013-08-07 16:05:43 -0700

    Added jquery and bootstrap js

--- index.php
+++ index.php
@@ -9,5 +9,11 @@ $exports["config"] = array(
             "./node_modules/hoobr-bootstrap/assets/css/bootstrap.min.css",
             "./node_modules/hoobr-theme-public/assets/css/main.css"
         )
+    ),
+    "js" => array(
+        "bottom" => array(
+            "./node_modules/hoobr-bootstrap/assets/js/jquery.min.js",
+            "./node_modules/hoobr-bootstrap/assets/js/bootstrap.min.js"
+        )
     )
 );
